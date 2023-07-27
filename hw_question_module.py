e21fa115f32e30279f41b7aba74861502003a25f
Omar Gonzalez <gomarzalez@gmail.com>
2023-07-27 14:02:12 -0700

    edited ability to del and added module hw function

--- hw_question_module.py
+++ hw_question_module.py
@@ -1,3 +1,25 @@
 
-def square_feet_of_house():
-    return 
+def square_feet_of_house(length, width):
+    room_area = length * width
+    return room_area
+
+length = float(input("enter the length in feet")) #int didnt work #str didnt work, process of illimination = float
+width = float(input("enter the width in feet"))
+
+
+total_sq_ft = square_feet_of_house(length, width)
+
+print(f'this is the total square footage of the house {total_sq_ft}')
+
+
+
+import math 
+
+def get_circumference():
+    circumference = 2 * math.pi * radius
+    return circumference 
+
+radius = input("what is the radius")
+
+circumference = get_circumference(radius)
+print(f'The total circumference for circle with {radius} is {circumference}')
