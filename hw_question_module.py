133f4bb818a92fe3e2d0791c4343312e46386e1f
Omar Gonzalez <gomarzalez@gmail.com>
2023-07-27 15:31:18 -0700

    updated del function to actually del

--- hw_question_module.py
+++ hw_question_module.py
@@ -1,25 +1,18 @@
 
-def square_feet_of_house(length, width):
-    room_area = length * width
-    return room_area
-
-length = float(input("enter the length in feet")) #int didnt work #str didnt work, process of illimination = float
-width = float(input("enter the width in feet"))
-
-
-total_sq_ft = square_feet_of_house(length, width)
-
-print(f'this is the total square footage of the house {total_sq_ft}')
-
-
-
 import math 
 
+
+def square_feet_of_house():
+    length = float(input("enter the length in feet")) #int didnt work #str didnt work, process of illimination = float
+    width = float(input("enter the width in feet"))
+    room_area = length * width
+    print(f'this is the total square footage of the house {total_sq_ft}')
+    return room_area
+
+
 def get_circumference():
+    radius = input("what is the radius")
     circumference = 2 * math.pi * radius
+    print(f'The total circumference for circle with {radius} is {circumference}')
     return circumference 
 
-radius = input("what is the radius")
-
-circumference = get_circumference(radius)
-print(f'The total circumference for circle with {radius} is {circumference}')
